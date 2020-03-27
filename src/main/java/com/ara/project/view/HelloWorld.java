886077f9628a9c57bf82e54adfb9d1536086c1fc
arafasaka <arafasaka@gmail.com>
2020-03-27 11:11:26 +0700

    Pertama

---
+++ src/main/java/com/ara/project/view/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.mulazi.project.view;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class HelloWorld {
+
+	private String input;
+	private String output;
+	public void submit() {
+		output = "Hello World! Ara Fasaka You have typed: " + input;
+	}
+	
+	public String getInput() {
+		return input;
+	}
+	
+	public void setInput(String input) {
+		this.input = input;
+	}
+	
+	public String getOutput() {
+		return output;
+	}
+}
